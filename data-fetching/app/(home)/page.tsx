4d07b1294f3ef0a7fb28280094e325791438f84b
오율산 (Yulsan Oh) <156407033+yulsanoh@users.noreply.github.com>
2024-07-25 13:40:28 +0900

    Update page.tsx

--- data-fetching/app/(home)/page.tsx
+++ data-fetching/app/(home)/page.tsx
@@ -1,21 +1,12 @@
-"use client";
+export const metadata = {
+  title: "Home",
+};
 
-import { useEffect, useState } from "react";
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (await fetch(`https://nomad-movies.nomadcoders.workers.dev/movies`)).json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
   return (
     <div>
-      {isLoading ? "Loading..." : JSON.stringify(movies)}
       <h1>hello</h1>
     </div>
   );
